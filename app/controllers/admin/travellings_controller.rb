e9970099d8a375c5a08a86a95d59de1ec9168853
Thanh Dat <tranlethanhdat1995@gmail.com>
2019-03-20 15:24:15 +0700

    demo

--- app/controllers/admin/travellings_controller.rb
+++ app/controllers/admin/travellings_controller.rb
@@ -1,7 +1,6 @@
 class Admin::TravellingsController < Admin::AdminBaseController
   before_action :check_permission
   before_action :load_travelling, only: %i(edit update destroy)
-<<<<<<< HEAD
   before_action :load_location, only: %i(edit new)
 
   def index
@@ -10,17 +9,6 @@ class Admin::TravellingsController < Admin::AdminBaseController
   end
 
   def edit; end
-=======
-
-  def index
-    @travellings = Travelling.paginate page: params[:page],
-      per_page: Settings.travelling_per_page
-  end
-
-  def edit
-    @locations = Location.all.map{|e| [e.name, e.id]}
-  end
->>>>>>> b0fff5c1fdc45cc8270e96f54be70a25df71a4ac
 
   def update
     if @travelling.update_attributes travelling_params
@@ -28,10 +16,7 @@ class Admin::TravellingsController < Admin::AdminBaseController
       redirect_to admin_travellings_path
     else
       flash[:danger] = t "update_travelling_failed"
-<<<<<<< HEAD
-=======
       redirect_to request.referrer
->>>>>>> b0fff5c1fdc45cc8270e96f54be70a25df71a4ac
     end
   end
 
@@ -41,18 +26,10 @@ class Admin::TravellingsController < Admin::AdminBaseController
     else
       flash[:danger] = t "del_travelling_failed"
     end
-<<<<<<< HEAD
-    redirect_to admin_travellings_path
-  end
-
-  def new
-=======
     redirect_to request.referrer
   end
 
   def new
-    @locations = Location.all.map{|e| [e.name, e.id]}
->>>>>>> b0fff5c1fdc45cc8270e96f54be70a25df71a4ac
     @travelling = Travelling.new
   end
 
@@ -62,12 +39,8 @@ class Admin::TravellingsController < Admin::AdminBaseController
       flash[:success] = t "add_travelling_success"
       redirect_to admin_travellings_path
     else
-<<<<<<< HEAD
-      render :new
-=======
       flash[:danger] = t "add_travelling_failed"
       redirect_to request.referrer
->>>>>>> b0fff5c1fdc45cc8270e96f54be70a25df71a4ac
     end
   end
 
@@ -80,13 +53,10 @@ class Admin::TravellingsController < Admin::AdminBaseController
     redirect_to admin_travellings_path
   end
 
-<<<<<<< HEAD
   def load_location
     @locations = Location.all.map{|e| [e.name, e.id]}
   end
 
-=======
->>>>>>> b0fff5c1fdc45cc8270e96f54be70a25df71a4ac
   def travelling_params
     params.require(:travelling).permit :location_start_id, :location_end_id
   end
--- app/controllers/travellings_controller.rb
+++ app/controllers/travellings_controller.rb
@@ -2,7 +2,6 @@ class TravellingsController < ApplicationController
   def index
     @travellings = Travelling.all.group_travellings
 
-<<<<<<< HEAD
     @travelling = if params[:location_end_id]
                     Travelling.where_travellings(params[:location_end_id])
                   else
@@ -13,12 +12,5 @@ class TravellingsController < ApplicationController
                  .where_time_start(Date.current)
                  .paginate(page: params[:page],
                    per_page: Settings.tours.per_page)
-=======
-    @tours = if params[:location_end_id]
-               Travelling.where_travellings(params[:location_end_id])
-             else
-               Travelling.all
-             end
->>>>>>> b0fff5c1fdc45cc8270e96f54be70a25df71a4ac
   end
 end
